aa9a05183be6585da9c5c52e91392de018f2d09e
JohnKaHill <John.Kallenberg@hhu.de>
2015-11-12 15:23:48 +0100

    Update statistics.cxx

--- statistics.cxx
+++ statistics.cxx
@@ -1,4 +1,4 @@
-#include <cmath>
+#include <ctime>
 #include <iostream>
 #include <cstdlib>
 
@@ -7,15 +7,34 @@ using namespace std;
 // plenty of lines here
 //--------------------------------------------------
 
+void randomize(double* R, const int N, double x){
+    for (int i=0; i<N; i++){
+        srand((unsigned)time(0));
+        x = rand()/(double)(RAND_MAX);
+        R[i]=x;
+    }
+}
+                 
+void calcmv(double* R, const int N, double& m, double& v){
+    for (int i=0; i<N; i++)
+        m = m + R[i];
+    m = m / N;
+    
+    for (int i=0; i<N; i++)
+        v = v + (R[i]-m)*(R[i]-m);
+    v = v / N;
+}
+
 int main(){
    const int N = 100;
-   double p[N];
+   double x=0, R[N], m=0, v=0;
    double mean, var;
-
+   randomize(R, N, x);
+   calcmv(R, N, m, v);
    // Some lines here....
 
-   cout << "Mean = " << mean << endl;
-   cout << "Variance = " << var << endl;
+   cout << "Mean = " << m << endl;
+   cout << "Variance = " << v << endl;
 
    return 0;
 }
